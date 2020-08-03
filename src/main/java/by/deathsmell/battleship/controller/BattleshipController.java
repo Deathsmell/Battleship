95aafb56a8cd726f472ffd7a6438715bed6dc080
deathsmell <xelasel.rotmen2010@gmail.com>
2020-08-03 21:18:25 +0300

    add websocket

--- src/main/java/by/deathsmell/battleship/controller/BattleshipController.java
+++ src/main/java/by/deathsmell/battleship/controller/BattleshipController.java
@@ -3,6 +3,8 @@ package by.deathsmell.battleship.controller;
 import by.deathsmell.battleship.Validators.BattleFieldValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,26 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BattleshipController {
 
-    @GetMapping("/field")
-    public int[][] getField() {
-        return new int[][]{
-                {1, 0, 0, 0, 0, 1, 1, 0, 0, 0}, // 0
-                {1, 0, 1, 0, 0, 0, 0, 0, 1, 0}, // 1
-                {1, 0, 1, 0, 1, 1, 1, 0, 1, 0}, // 2
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 3
-                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, // 4
-                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, // 5
-                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, // 6
-                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, // 7
-                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, // 8
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}  // 9
-        };
-    }
-
     @PostMapping("/validationField")
     public ResponseEntity<HttpStatus> validationField(@RequestBody int[][] field) {
         boolean valid = BattleFieldValidator.fieldValidator(field);
         return valid ? ResponseEntity.ok(HttpStatus.OK) : ResponseEntity.badRequest().build();
     }
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting (String message){
+        return message;
+    }
+
 }
 
