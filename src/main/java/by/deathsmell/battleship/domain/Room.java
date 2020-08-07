16dc67ee80edadedc007bc1bba08710f1781d558
deathsmell <xelasel.rotmen2010@gmail.com>
2020-08-07 15:40:20 +0300

    add Spring Data and create some entity

--- src/main/java/by/deathsmell/battleship/domain/Room.java
+++ src/main/java/by/deathsmell/battleship/domain/Room.java
@@ -3,8 +3,28 @@ package by.deathsmell.battleship.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
 @Data
 @NoArgsConstructor
-public class Room {
-    //empty
+public class Room implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private UUID room;
+    private String player1;
+    private String player2;
+    private State state;
+
+    public enum State {
+        CREATE, WAIT,FILED, DESTROY
+    }
 }
---
+++ src/main/java/by/deathsmell/battleship/repositories/RoomRepository.java
@@ -0,0 +1,11 @@
+package by.deathsmell.battleship.repositories;
+
+import by.deathsmell.battleship.domain.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
+
+    Room findByRoom(UUID roomId);
+}
